eabb075910e06c47187d22e598af9a4c1e1c3a79
RichardPostema <r.postema.2@st.hanze.nl>
2025-03-20 10:39:39 +0100

    Ik heb titel toegevoegd aan mijn overzicht

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Astronauten overzicht</h1>
         <Astronauts />
         <Launches />
       </main>
