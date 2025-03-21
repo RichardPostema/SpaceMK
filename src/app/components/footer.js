192f3fd05e47d18a5641acb0be1d57a9e7096a5c
RichardPostema <r.postema.2@st.hanze.nl>
2025-03-21 17:13:39 +0100

    Update footer.js

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,8 +1,9 @@
 export default function Footer() {
-    return (
-      <footer>
-        <p>Ontwerp door Richard Postema</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer style={{ textAlign: "center" }}>
+      <p>
+        Ontwerp door <a href="https://richardpostema.nl" target="_blank" rel="noopener noreferrer">Richard Postema</a>
+      </p>
+    </footer>
+  );
+}
